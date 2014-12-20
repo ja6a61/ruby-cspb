d471a781a1dba54cc4b95be3cec2bbca3a30459b
Jason Angus <ja6a61@gmail.com>
2014-12-21 04:52:14 +0800

    working on Board class.

--- ch11/ex11-1.rb
+++ ch11/ex11-1.rb
@@ -27,8 +27,77 @@ class Board
   def display
     puts @board.to_s
   end
+
+  def store (row, col, colour)
+    # validate input
+    if row >= 0 and row < MAX_ROWS
+      if col >= 0 and col < MAX_COLS
+        if colour.upcase == "B" or colour.upcase == "W"
+          @board[row][col] = colour
+          return true
+        else
+          return false
+        end
+      end
+    end
+  end
 end
 
+def get_row
+  input_good = false
+  row_input = ""
+
+  while input_good == false
+    print "Enter row (1-19): "
+    row_input = gets.to_i
+    # validate int between 1 and 19
+    if row_input >= 1 and row_input <= 19
+      row_input -= 1
+      input_good = true
+    else
+      puts "Row must be between 1 and 19. Try again."
+    end
+  end
+  return row_input
+end
+
+def get_col
+  input_good = false
+  col_input = ""
+
+  while input_good == false
+    print "Enter column (1-19): "
+    col_input = gets.to_i
+    # validate int between 1 and 19
+    if col_input >= 1 and col_input <= 19
+      col_input -= 1
+      input_good = true
+      return col_input
+    else
+      puts "Column must be between 1 and 19. Try again."
+    end
+  end
+end
+
+def get_colour
+  input_good = false
+  colour_input = ""
+
+  while input_good == false
+    print "Enter colour (B/W): "
+    colour_input = gets
+    colour_input.chomp!
+    # validate input either 'B' or 'W'
+    if colour_input.upcase == "B" or colour_input.upcase == "W"
+      input_good = true
+      colour_input.upcase!
+      return colour_input
+    else
+      puts "Colour must be either 'B' or 'W'."
+    end
+  end
+  return colour_input
+end
 
 board = Array.new(MAX_ROWS) {Array.new(MAX_COLS)}
 #puts board.to_s
@@ -41,54 +110,28 @@ my_board.display
 # Place moves in to the array.
 # Save the array representing the state of the board to a file.
 #
-input_good = false
 
-while input_good == false
-  print "Enter row (1-19): "
-  row_input = gets.to_i
-  # validate int between 1 and 19
-  if row_input >= 1 and row_input <= 19
-    row_input -= 1
-    input_good = true
-  else
-    puts "Row must be between 1 and 19. Try again."
+continue = true
+decision = ""
+while continue == true
+  r_ip = get_col
+  c_ip = get_row
+  clr_ip = get_colour
+  my_board.store(r_ip, c_ip, clr_ip)
+  while decision != "Y" and decision != "N"
+    print "Enter another move? (y/n): "
+    decision = gets.chomp.upcase
+    if decision == "N"
+      continue = false
+    end
   end
+  decision = ""
 end
 
-input_good = false
-
-while input_good == false
-  print "Enter column (1-19): "
-  col_input = gets.to_i
-  # validate int between 1 and 19
-  if col_input >= 1 and col_input <= 19
-    col_input -= 1
-    input_good = true
-  else
-    puts "Column must be between 1 and 19. Try again."
-  end
-end
-
-input_good = false
-
-while input_good == false
-  print "Enter colour (B/W): "
-  colour_input = gets
-  colour_input.chomp!
-  # validate input either 'B' or 'W'
-  if colour_input.upcase == "B" or colour_input.upcase == "W"
-    input_good = true
-    colour_input.upcase!
-  else
-    puts "Colour must be either 'B' or 'W'."
-  end
-end
-
-board[row_input][col_input] = colour_input
-
-puts "row_input: " + row_input.to_s
-puts "col_input: " + col_input.to_s
-puts "colour_input: " + colour_input
-puts "board[#{row_input}][#{col_input}]: " + board[row_input][col_input]
+#puts "row_input: " + row_input.to_s
+#puts "col_input: " + col_input.to_s
+#puts "colour_input: " + colour_input
+#puts "board[#{row_input}][#{col_input}]: " + board[row_input][col_input]
+my_board.display
 
 

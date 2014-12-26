85b466eec6b056e43df25d4a17e7d982c6171431
Jason Angus <ja6a61@gmail.com>
2014-12-27 07:52:47 +0800

    Ex11-1 write a program to read words with definitions form a file.

---
+++ ch11/ex11-2.rb
@@ -0,0 +1,11 @@
+# ex11-2.rb
+# sat 27 dec 2014
+#
+# Write a program that quizzes the user on her or his vocabulary. Make the 
+# program read a set of words and definitions from a file; display definitions 
+# one at a time, in random order; and prompt for the appropriate word.
+#
+require_relative "file_writer.rb"
+# write a reader class for reading files
+# require_relative "file_reader.rb"
+#
---
+++ ch11/file_reader.rb
@@ -0,0 +1,20 @@
+# file_reader.rb
+# sat 27 dec 2014
+#
+# The class to encapsulates writing a file.
+#
+class FileReader
+
+  def initialize(file_name)
+    @file = File.open(file_name, "r")
+  end
+
+  def read_line
+    # Hopefully this method returns the last line read
+   @file.gets
+  end
+
+  def close
+    @file.close
+  end
+end
